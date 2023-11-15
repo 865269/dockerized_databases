b4a6bf47e0e80ceff435cadad9541bdb08c8db92
Matthew Diggory <mattydiggory@gmail.com>
2023-11-15 17:44:41 +0000

    add base project

---
+++ src/main/java/com/matthew/dockerized_databases/DockerizedDatabasesApplication.java
@@ -0,0 +1,13 @@
+package com.matthew.dockerized_databases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizedDatabasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizedDatabasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthew/dockerized_databases/DockerizedDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew.dockerized_databases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedDatabasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
